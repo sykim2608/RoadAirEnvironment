b1ced65fb1da72174a883843dc1143f6cdeb64c2
Soyoung Kim <sykim2608@gmail.com>
2020-08-11 10:43:17 +0900

    feat: armcharts를 이용한 대기 환경 측정값 출력 - 도로변 및 측정소 조회 ajax 요청의 응답 데이터 내의 대기환경 측정값을 armcharts를 활용하여 화면에 출력

--- src/main/java/com/ntels/sykim/RoadAirEnvironment/model/SearchParam.java
+++ src/main/java/com/ntels/sykim/RoadAirEnvironment/model/SearchParam.java
@@ -1,7 +1,17 @@
 package com.ntels.sykim.RoadAirEnvironment.model;
 
+/**
+ * 도로변 및 측정소 구분에 따른 조회 파라미터 모델
+ * @author sykim@ntels.com
+ */
 public class SearchParam {
+    /**
+     * 도로변 구분 명
+     */
     private String RoadCategory;
+    /**
+     * 측정소 명
+     */
     private String RoadStatus;
 
     public String getRoadCategory() {
